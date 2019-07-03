f0bc6cb5330b37521e07ccd7ae23dfce58b8a16c
Ernesto Acevedo <eacevedo@debian.cl>
2019-07-03 11:49:45 -0400

    Add single ticket query

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -1,9 +1,16 @@
 module Types
   class QueryType < Types::BaseObject
     field :all_tickets, [TicketType], null: false
+    field :ticket, [TicketType], null: false do
+      argument :id, String, required: true
+    end
 
     def all_tickets
       Ticket.all
+    end
+
+    def ticket(id: nil)
+      Ticket.where(id)
     end
   end
 end
